e546011470824b1cb6bdeab7deff5525ecc2c7b6
Ram <rmailtat777@gmail.com>
2023-02-24 12:48:32 +0300

    Gotovo

---
+++ task_34/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+}
+
+void FindEven(int[] numbers, out int amount)
+{
+    amount = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] % 2 == 0)
+        {
+            amount++;
+        }
+    }
+    System.Console.WriteLine($"Количество четных чисел в массиве: {amount}");
+}
+
+
+int[] list = GetArray(7);
+PrintArray(list);
+FindEven(list, out int amount);
